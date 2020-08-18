aade39bfa3a7cdc660a6c1f1d5ec2ff0963d0e95
Ruud van der Weide <rj@rdcl.dev>
2020-08-18 16:04:36 +0200

    Zip observables into a single observable

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import { filter, map, scan } from 'rxjs/operators'
 import { replayRecordsFrom } from './lib/fit'
-import { OperatorFunction, pipe } from 'rxjs'
+import { OperatorFunction, pipe, zip } from 'rxjs'
 
 if (process.argv.length < 3) {
   console.error(`Usage: ${ process.argv[0] } ${ process.argv[1] } <FILE>`)
@@ -10,11 +10,20 @@ if (process.argv.length < 3) {
 
 const file = path.resolve(process.cwd(), process.argv[2])
 const records$ = replayRecordsFrom(file)
+
+const cadence$ = records$.pipe(map(r => r.cadence))
 const speed$ = records$.pipe(map(r => r.speed))
 const heartRate$ = records$.pipe(map(r => r.heart_rate))
 
-speed$.pipe(slidingAverage(10)).subscribe(console.log)
-heartRate$.pipe(slidingAverage(10)).subscribe(console.log)
+const avgCadance$ = cadence$.pipe(slidingAverage(10))
+const avgSpeed$ = speed$.pipe(slidingAverage(10))
+const avgHeartRate$ = heartRate$.pipe(slidingAverage(10))
+
+zip(avgCadance$, avgSpeed$, avgHeartRate$)
+  .pipe(
+    map(([cadance, speed, heartRate]) => ({ cadance, speed, heartRate })),
+  )
+  .subscribe(console.log)
 
 function slidingAverage(windowSize: 10): OperatorFunction<number, number> {
   return pipe(
